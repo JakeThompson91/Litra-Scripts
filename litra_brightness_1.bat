d7c2a32ae8e56d448043f0df3678508931b89bd8
Jake <jat09g@gmail.com>
2024-06-03 20:50:12 -0400

    adding batch files

---
+++ litra_brightness_1.bat
@@ -0,0 +1,3 @@
+chdir /d "D:\Program Files\hidapitester"
+
+hidapitester.exe --vidpid 046d/c900 --usagePage ff43 --open --send-output 0x11,0xFF,0x04,0x4F,0x00,0x1C
---
+++ litra_temp_3.bat
@@ -0,0 +1,3 @@
+chdir /d "D:\Program Files\hidapitester"
+
+hidapitester.exe --vidpid 046d/c900 --usagePage ff43 --open --send-output 0x11,0xFF,0x04,0x9D,0x11,0x30
